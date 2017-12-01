03b8be211ee0a98880b1171a237af6ef6687ec4e
Eric Jones <erjones@tardis.usersys.redhat.com>
2017-12-01 14:57:31 -0500

    Cleaned up the code. It now works totally from command line  and even checks if it is a number and proceeds if it is and fails out if it is not.

--- day1_exercise1.py
+++ day1_exercise1.py
@@ -1,4 +1,11 @@
-b =
+#!/usr/bin/env python3
+from sys import argv
+
+try:
+  b = int(argv[1])
+except:
+  print("NOPEEEE, {0} is not a number!".format(argv[1]))
+  exit()
 info = []
 for list in range(0,len(str(b))):
   try:
@@ -15,9 +22,7 @@ for list in range(0,len(str(b))):
     else:
       #"no, because {0} is != to {1}".format(str(b)[list],str(b)[0])
       pass
-
 total = 0
 for place in range(0,len(info)):
   total = total + int(info[place])
-
 print(total)
